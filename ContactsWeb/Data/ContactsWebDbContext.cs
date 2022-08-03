1f8d7be2758e74ac5c2c2bbb07aecae056f9871b
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-08-03 09:33:33 -0300

    Configuracao do DbContext

---
+++ ContactsWeb/Data/ContactsWebDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using ContactsWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsWeb.Data
+{
+    public class ContactsWebDbContext : DbContext
+    {
+        public ContactsWebDbContext(DbContextOptions<ContactsWebDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
