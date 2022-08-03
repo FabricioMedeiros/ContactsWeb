7d68c482e73127a9c80905f65430372957d9bbaa
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-08-03 09:19:49 -0300

    Criacao das Models Contact e Address

---
+++ ContactsWeb/Models/Address.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsWeb.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int ContactId { get; set; }       
+
+        [Display(Name ="Endereço")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} o tamanho dever ter entre {2} e {1} caracteres")]
+        public string Street { get; set; }
+
+        [Display(Name = "Número")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(10, MinimumLength = 1, ErrorMessage = "{0} o tamanho dever ter entre {2} e {1} caracteres")]
+        public string Number { get; set; }
+
+        [Display(Name = "Complemento")]
+        public string Complement { get; set; }      
+
+        [Display(Name = "Bairro")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "{0} o tamanho dever ter entre {2} e {1} caracteres")]
+        public string Neighborhood { get; set; }
+
+        [Display(Name = "Cidade")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(60, MinimumLength = 2, ErrorMessage = "{0} o tamanho dever ter entre {2} e {1} caracteres")]
+        public string City { get; set; }
+
+        [Display(Name = "Estado")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "{0} o tamanho dever ter entre {2} e {1} caracteres")]
+        public string State { get; set; }
+
+        [Display(Name = "Cep")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "{0} precisa ter {1} caracteres")]
+        public string ZipCode { get; set; }
+
+        public Contact Contact { get; set; }
+    }
+}
---
+++ ContactsWeb/Models/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactsWeb.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Data Cadastro")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime RegistreDate { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} o tamanho dever ter entre {2} e {1} caracteres")]
+        public String Name { get; set; }
+
+        [Display(Name = "Telefone")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(15, MinimumLength = 14, ErrorMessage = "{0} o tamanho dever ter entre {2} e {1} caracteres")]
+        public String Phone { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Informe um Email válido")]
+        public String Email { get; set; }
+
+        public Address Address { get; set; }
+    }
+}
