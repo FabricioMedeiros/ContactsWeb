acf39fa1fcf25789dd3156dcbdde5ea45f3c5bcd
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-08-03 09:43:52 -0300

    Criacao da Migration Inicial

---
+++ ContactsWeb/Migrations/20220803124221_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactsWeb.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegistreDate = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(maxLength: 60, nullable: false),
+                    Phone = table.Column<string>(maxLength: 15, nullable: false),
+                    Email = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactId = table.Column<int>(nullable: false),
+                    Street = table.Column<string>(maxLength: 60, nullable: false),
+                    Number = table.Column<string>(maxLength: 10, nullable: false),
+                    Complement = table.Column<string>(nullable: true),
+                    Neighborhood = table.Column<string>(maxLength: 25, nullable: false),
+                    City = table.Column<string>(maxLength: 60, nullable: false),
+                    State = table.Column<string>(maxLength: 20, nullable: false),
+                    ZipCode = table.Column<string>(maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_ContactId",
+                table: "Addresses",
+                column: "ContactId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
