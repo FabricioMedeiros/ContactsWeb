2c452c0ef25a8b4e17775e29798073a19142d33f
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-08-07 10:27:12 -0300

    Criacao do Metodo RemoveMask para remover a mascara de Telefone e Cep

---
+++ ContactsWeb/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactsWeb.Models
+{
+    public static class StringExtensions
+    {
+        public static string RemoveMask(this string value)
+        {
+            return value.Replace(" ", string.Empty).Replace(".", string.Empty).Replace("-", string.Empty).Replace("/", string.Empty).Replace("(", string.Empty).Replace(")", string.Empty);
+        }
+    }
+}
