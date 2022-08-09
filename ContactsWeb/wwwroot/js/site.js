00a2f6c57071179cd778b28884c285aea5de8fd3
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-08-09 19:47:27 -0300

    Criacao da funcao SearchZipCode para consultar o Cep via WebService

--- ContactsWeb/wwwroot/js/site.js
+++ ContactsWeb/wwwroot/js/site.js
@@ -24,3 +24,51 @@
     });
 
 jQuery("input.zipcode").mask("99.999-999");
+
+function SearchZipCode() {
+    $(document).ready(function () {
+
+        function clear_fields() {
+            $("#Address_Street").val("");
+            $("#Address_Neighborhood").val("");
+            $("#Address_City").val("");
+            $("#Address_State").val("");
+        }
+
+        $("#Address_ZipCode").blur(function () {
+            var zipcode = $(this).val().replace(/\D/g, '');
+
+            if (zipcode != "") {
+                var checkzipcode = /^[0-9]{8}$/;
+
+                if (checkzipcode.test(zipcode)) {
+                    $("#Address_Street").val("...");
+                    $("#Address_Neighborhood").val("...");
+                    $("#Address_City").val("...");
+                    $("#Address_State").val("...");
+
+                    $.getJSON("https://viacep.com.br/ws/" + zipcode + "/json/?callback=?",
+                        function (data) {
+                            if (!("erro" in data)) {
+                                $("#Address_Street").val(data.logradouro);
+                                $("#Address_Neighborhood").val(data.bairro);
+                                $("#Address_City").val(data.localidade);
+                                $("#Address_State").val(data.uf);
+                            }
+                            else {
+                                clear_fields();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                }
+                else {
+                    clear_fields();
+                    alert("Formato de CEP inválido.");
+                }
+            }
+            else {
+                clear_fields();
+            }
+        });
+    });
+}
