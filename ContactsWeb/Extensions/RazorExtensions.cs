e2ba106c814f6f1b9833a2848ace60db983f9724
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-08-13 14:05:08 -0300

    Criacao do Metodo FormatPhone para formatar mascaras de telefone em Razor Pages

---
+++ ContactsWeb/Extensions/RazorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace ContactsWeb.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormatPhone(this RazorPage page, string phone)
+        {
+             return phone.Length > 10 ? string.Format("{0:(##) #####-####}", Int64.Parse(phone)) : string.Format("{0:(##) ####-####}", Int64.Parse(phone));
+        }
+    }
+}
